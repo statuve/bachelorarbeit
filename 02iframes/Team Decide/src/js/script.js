0485b6f2b1c254e40b4b32e2db0921d38edcebe5
Florian Metz <statuve@gmx.de>
2021-01-07 20:25:06 +0100

    communication #8

--- 02iframes/Team Decide/src/js/script.js
+++ 02iframes/Team Decide/src/js/script.js
@@ -2,7 +2,7 @@ const bc = new BroadcastChannel('eventChannel');
 
 console.log(bc.name);
 
-window.onload(bc.postMessage("wtf"));
+bc.postMessage("wtf");
 
 bc.onmessage = function (ev) {
     console.log("jaja");
