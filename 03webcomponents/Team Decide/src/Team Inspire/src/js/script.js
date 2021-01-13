d20b608b147ab74ea7ae2812175681a70ac356e8
Florian Metz <statuve@gmx.de>
2021-01-13 14:49:58 +0100

    assets bereinigung

--- 03webcomponents/Team Decide/src/Team Inspire/src/js/script.js
+++ 03webcomponents/Team Decide/src/Team Inspire/src/js/script.js
@@ -5,11 +5,11 @@ class Recommendations extends HTMLElement {
     <style> @import "css/bootstrap.css"; </style>
 
       <p>Ebenfalls interessant:</p>
-      <img src="assets/rsz_seagate_backup-plus-slim_1-2tb_rot_front.jpg" class="img-fluid" alt="Responsive image">
-      <img src="assets/rsz_toshiba_blau.jpg" class="img-fluid" alt="Responsive image">
-      <img src="assets/rsz_verbatim_gold.jpg" class="img-fluid" alt="Responsive image">
-      <img src="assets/rsz_zinc_lila.jpg" class="img-fluid" alt="Responsive image">
-      <img src="assets/rsz_platinum_grün.jpg" class="img-fluid" alt="Responsive image">  
+      <img src="Team Inspire/src/assets/rsz_seagate_backup-plus-slim_1-2tb_rot_front.jpg" class="img-fluid" alt="Responsive image">
+      <img src="Team Inspire/src/assets/rsz_toshiba_blau.jpg" class="img-fluid" alt="Responsive image">
+      <img src="Team Inspire/src/assets/rsz_verbatim_gold.jpg" class="img-fluid" alt="Responsive image">
+      <img src="Team Inspire/src/assets/rsz_zinc_lila.jpg" class="img-fluid" alt="Responsive image">
+      <img src="Team Inspire/src/assets/rsz_platinum_grün.jpg" class="img-fluid" alt="Responsive image">  
       `;
     }
   }
